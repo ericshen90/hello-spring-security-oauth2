b33f7eb5cb7c2a70e31a6f739fb9a4adc79895d6
EricShen <ahsbt@126.com>
2019-12-10 15:25:22 +0800

    基于 JDBC 存储令牌

--- hello-spring-security-oauth2-server/src/main/java/com/eric/spring/security/oauth2/server/config/AuthorizationServerConfiguration.java
+++ hello-spring-security-oauth2-server/src/main/java/com/eric/spring/security/oauth2/server/config/AuthorizationServerConfiguration.java
@@ -1,11 +1,21 @@
 package com.eric.spring.security.oauth2.server.config;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 /**
  * 认证服务器
@@ -20,21 +30,35 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
   @Resource
   private BCryptPasswordEncoder passwordEncoder;
 
+  @Bean
+  @Primary
+  @ConfigurationProperties(prefix = "spring.datasource")
+  public DataSource dataSource() {
+    // 配置数据源（使用的是 HikariCP 连接池），以上注解是指定数据源，否则会有冲突
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    // 基于JDBC实现，令牌保存到数据库
+    return new JdbcTokenStore(dataSource());
+  }
+
+  @Bean
+  public ClientDetailsService jdbcClientDetailsService() {
+    // 基于JDBC实现，需要事先在数据库配置客户端信息
+    return new JdbcClientDetailsService(dataSource());
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    // 设置令牌存储模式
+    endpoints.tokenStore(tokenStore());
+  }
+
   @Override
   public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
     // 配置客户端
-    clients
-        // 使用内存设置
-        .inMemory()
-        // 客户端id
-        .withClient("client")
-        // 客户端密码
-        .secret(passwordEncoder.encode("secret"))
-        // 授权类型
-        .authorizedGrantTypes("authorization_code")
-        // 授权范围
-        .scopes("app")
-        // 注册回调地址
-        .redirectUris("https://www.google.com");
+    clients.withClientDetails(jdbcClientDetailsService());
   }
 }
---
+++ hello-spring-security-oauth2-server/src/test/java/OAuth2ServerApplicationTests.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author EricShen
+ * @date 2019-12-10
+ */
+// @SpringBootTest
+@RunWith(SpringRunner.class)
+public class OAuth2ServerApplicationTests {
+
+  @Test
+  public void testPasswordEncoder() {
+    System.out.println("======================================================");
+    System.out.println(new BCryptPasswordEncoder().encode("secret"));
+  }
+
+}
